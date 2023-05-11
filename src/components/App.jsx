6fb4d68b865792df6649e14404ce16cf58c1d8a4
Zerossik <zeross19940303@gmail.com>
2023-05-11 20:51:24 +0300

    main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,4 @@
 import { Component } from 'react';
-import { Statistics } from './Statistics/Statistics';
-import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
-import { Section } from './Section/Section';
 
 export class App extends Component {
   state = {};
