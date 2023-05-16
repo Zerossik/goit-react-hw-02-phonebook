85429f553a8cf4801cc25dee480058505311bdb0
Zerossik <zeross19940303@gmail.com>
2023-05-16 19:47:42 +0300

    PT add

--- src/components/ContactsList/ContactListItem.jsx
+++ src/components/ContactsList/ContactListItem.jsx
@@ -14,4 +14,5 @@ export function ContactListItem({ contacts, deleteContact }) {
 }
 ContactListItem.propTypes = {
   contacts: PropTypes.func.isRequired,
+  deleteContact: PropTypes.func.isRequired,
 };
